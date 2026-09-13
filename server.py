d12493fda25fa1c83a0da0d746883f483ecda46b
shadowXTRCosmox <geceremir319@gmail.com>
2026-09-13 21:50:30 +0300

    Update server.py

--- server.py
+++ server.py
@@ -5,7 +5,7 @@ import psycopg2
 import websockets
 
 # --- VERİTABANI BAĞLANTISI ---
-DATABASE_URL = os.environ.get("DATABASE_URL") or "postgresql://postgres.qbpnqccxvacbgcaioizf:emir8514%2112@aws-0-ap-northeast-1.pooler.supabase.com:6543/postgres"
+DATABASE_URL = "postgresql://postgres.qbpnqccxvacbgcaioizf:emir8514%2112@aws-0-ap-northeast-1.pooler.supabase.com:6543/postgres"
 
 conn = psycopg2.connect(DATABASE_URL)
 cursor = conn.cursor()
